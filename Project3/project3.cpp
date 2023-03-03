a5badf44948d441d455a7e32a2224c41b4423caa
Preston Roesslet <proesslet@gmail.com>
2023-03-03 17:54:48 -0600

    Project 3 started

--- Project3/project3.cpp
+++ Project3/project3.cpp
@@ -1,53 +1,171 @@
-// Project 3 
+// Project 3
 // CS 2413 Data Structures
 // Spring 2023
 
 #include <iostream>
 #include <vector> // for array of transactions
-#include <list> // for blockchain
+#include <list>	  // for blockchain
 
 using namespace std;
 
 class transaction
 {
-	int tID; // transaction id
-	int fromID; // source ID
-	int toID; // target ID
-	int tAmount; // how much is being transfered
+	int tID;		  // transaction id
+	int fromID;		  // source ID
+	int toID;		  // target ID
+	int tAmount;	  // how much is being transfered
 	string timeStamp; // time of the transaction read from the input file
 
-	public: 
-		transaction(); // default constructor
-		transaction(int temptID, int tempfromID, int temptoID, int temptAmount, int temptimeStamp); // non default constructor - default 100 for values
-		// all setters and getters
+public:
+	// default constructor
+	transaction();
+	// non default constructor
+	transaction(int temptID, int tempfromID, int temptoID, int temptAmount, string temptimeStamp)
+	{
+		tID = temptID;
+		fromID = tempfromID;
+		toID = temptoID;
+		tAmount = temptAmount;
+		timeStamp = temptimeStamp;
+	}
+	// non default constructor - default 100 for values
+	transaction(int temptID, int tempfromID, int temptoID, int temptAmount)
+	{
+		tID = temptID;
+		fromID = tempfromID;
+		toID = temptoID;
+		tAmount = temptAmount;
+		timeStamp = "00:00:00";
+	}
+	// all setters and getters
+	void setID(int temptID)
+	{
+		tID = temptID;
+	}
+	void setFromID(int tempfromID)
+	{
+		fromID = tempfromID;
+	}
+	void setToID(int temptoID)
+	{
+		toID = temptoID;
+	}
+	void setAmount(int temptAmount)
+	{
+		tAmount = temptAmount;
+	}
+	void setTimeStamp(string temptimeStamp)
+	{
+		timeStamp = temptimeStamp;
+	}
+	int getID()
+	{
+		return tID;
+	}
+	int getFromID()
+	{
+		return fromID;
+	}
+	int getToID()
+	{
+		return toID;
+	}
+	int getAmount()
+	{
+		return tAmount;
+	}
+	string getTimeStamp()
+	{
+		return timeStamp;
+	}
 };
 
 class block
 {
-	int blockNumber; // block number of the current block
-	int currentNumTransactions; // how many transactions are currently in the block
-	int maxNumTransactions; // how many maximum transactions can be in the block
+	int blockNumber;				   // block number of the current block
+	int currentNumTransactions;		   // how many transactions are currently in the block
+	int maxNumTransactions;			   // how many maximum transactions can be in the block
 	vector<transaction> bTransactions; // vector of transaction objects
 
-	public:
-		block(); // default constructor
-		block(int bNumber, int maxTransactions); // non default constructor
-		// search method for searching through array of transactions
-		// insert method to insert a new transaction
-		// setters and getters as needed
+public:
+	block();								 // default constructor
+	block(int bNumber, int maxTransactions); // non default constructor
+	// search method for searching through array of transactions
+	void search(int tID)
+	{
+		for (int i = 0; i < bTransactions.size(); i++)
+		{
+			if (bTransactions[i].getID() == tID)
+				cout << "Transaction ID: " << bTransactions[i].getID() << " From ID: " << bTransactions[i].getFromID() << " To ID: " << bTransactions[i].getToID() << " Amount: " << bTransactions[i].getAmount() << " Time Stamp: " << bTransactions[i].getTimeStamp() << endl;
+			else
+				cout << "Transaction ID: " << tID << " not found" << endl;
+		}
+	}
+	// insert method to insert a new transaction
+	void insert(transaction t1)
+	{
+		bTransactions.push_back(t1);
+		currentNumTransactions++;
+	}
+	// setters and getters as needed
+	void setCurrentNumTransactions(int cNumTransactions)
+	{
+		currentNumTransactions = cNumTransactions;
+	}
+	int getBlockNumber()
+	{
+		return blockNumber;
+	}
+	int getCurrentNumTransactions()
+	{
+		return currentNumTransactions;
+	}
+	int getMaxNumTransactions()
+	{
+		return maxNumTransactions;
+	}
 };
 
 class blockChain
 {
 	int currentNumBlocks; // maintain the size of the list/block chain list
-	list<block> bChain; // blockchain as a linked list
+	list<block> bChain;	  // blockchain as a linked list
 
-	public: 
-		blockChain(); // default constructor
-		blockChain(int tPerB); // non default constructor
-		// insert method to insert new transaction into the block chain - add new block if existing block is full
-		// while inserting new block into list, insert front 
-		// setters and getters as needed
+public:
+	blockChain(); // default constructor
+	// non default constructor
+	blockChain(int tPerB)
+	{
+		block b1(0, tPerB);
+		bChain.push_front(b1);
+		currentNumBlocks = 1;
+	}
+	// insert method to insert new transaction into the block chain - add new block if existing block is full
+	// while inserting new block into list, insert front
+	void insert(transaction t1)
+	{
+		if (bChain.front().getCurrentNumTransactions() == bChain.front().getMaxNumTransactions())
+		{
+			block b1(currentNumBlocks, bChain.front().getMaxNumTransactions());
+			b1.insert(t1);
+			bChain.push_front(b1);
+			currentNumBlocks++;
+		}
+		else
+		{
+			bChain.front().insert(t1);
+		}
+	}
+
+	// setters and getters as needed
+	void setCurrentNumBlocks(int cNumBlocks)
+	{
+		currentNumBlocks = cNumBlocks;
+	}
+	int getCurrentNumBlocks()
+	{
+		return currentNumBlocks;
+	}
 };
 
 int main()
@@ -63,10 +181,20 @@ int main()
 	cout << "Total number of transactions: " << totalNumTransactions << endl;
 
 	// object of block chain
-	blockChain* b1 = new blockChain(numTransactionsPerBlock);
+	blockChain *b1 = new blockChain(numTransactionsPerBlock);
 
 	// insert transactions into the blockchain
+	for (int i = 0; i < totalNumTransactions; i++)
+	{
+		int tID, fromID, toID, tAmount;
+		string timeStamp;
+
+		cin >> tID >> fromID >> toID >> tAmount >> timeStamp;
+
+		transaction *t1 = new transaction(tID, fromID, toID, tAmount, timeStamp);
+
+		b1->insert(*t1);
+	}
 
 	return 0;
 }
-
