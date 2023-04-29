4cd46f79de4cc7c0f1725976da3a1f03ac8e47ac
Preston Roesslet <proesslet@gmail.com>
2023-04-28 20:28:16 -0500

    Project 6 somewhat finished

--- Project6/project6.cpp
+++ Project6/project6.cpp
@@ -7,16 +7,50 @@
 #include <fstream> // for reading a file again and again
 #include <map>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 // Hash Function
-void hashFunction(string s)
+int hashFunction(string s)
 {
-	// TODO: Write hash function
+	// Write hash function for string using chaining (?) to handle collisions
+	// Mod 26
+	int hash_value = 0;
+	int ascii_value = (int)s[0];
+	hash_value = ascii_value % 26;
+
+	return hash_value;
 }
 
-// write display functions for map and unordered map
+// Display functions for map and unordered map
+void displayMap(map<int, vector<string>> m)
+{
+	cout << "Map:" << endl;
+	for (auto it = m.begin(); it != m.end(); it++)
+	{
+		cout << it->first << ": ";
+		for (int i = 0; i < it->second.size(); i++)
+		{
+			cout << it->second[i] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void displayUnorderedMap(unordered_map<int, vector<string>> m)
+{
+	cout << "Unordered map" << endl;
+	for (auto it = m.begin(); it != m.end(); it++)
+	{
+		cout << it->first << ": ";
+		for (int i = 0; i < it->second.size(); i++)
+		{
+			cout << it->second[i] << " ";
+		}
+		cout << endl;
+	}
+}
 
 using namespace std;
 
@@ -39,12 +73,42 @@ int main()
 	string temp;
 	// reading and performing operations
 	// start timer
+	clock_t start = clock();
 	while (fI >> option)
 	{
 		fI >> temp;
 		// create the structure - map - m1
+		if (option == 'I')
+		{
+			int temp2 = hashFunction(temp);
+			// if the key already exists, then add the value to the vector
+			if (m1.find(temp2) != m1.end())
+			{
+				m1[temp2].push_back(temp);
+			}
+			// if the key does not exist, then create a new key and add the value to the vector
+			else
+			{
+				m1.insert(pair<int, vector<string>>(temp2, {temp}));
+			}
+		}
+		else if (option == 'R')
+		{
+			// if the key exists, then remove the value from the vector
+			vector<string> temp3 = m1[hashFunction(temp)];
+			for (int i = 0; i < temp3.size(); i++)
+			{
+				if (temp3[i] == temp)
+				{
+					temp3.erase(temp3.begin() + i);
+				}
+			}
+		}
 	}
 	// stop timer
+	clock_t stop = clock();
+	double elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
+	cout << "Elapsed time for map in ms: " << elapsed << endl;
 
 	// close the opened file
 	fI.close();
@@ -55,15 +119,46 @@ int main()
 	fI.open(fileName);
 
 	// start timer
+	start = clock();
 	while (fI >> option)
 	{
 		fI >> temp;
 		// create the structure - unordered map - m2
+		if (option == 'I')
+		{
+			// if the key already exists, then add the value to the vector
+			if (m2.find(hashFunction(temp)) != m2.end())
+			{
+				m2[hashFunction(temp)].push_back(temp);
+			}
+			// if the key does not exist, then create a new key and add the value to the vector
+			else
+			{
+				m2.insert(pair<int, vector<string>>(hashFunction(temp), {temp}));
+			}
+		}
+		else if (option == 'R')
+		{
+			// if the key exists, then remove the value from the vector
+			vector<string> temp3 = m2[hashFunction(temp)];
+			for (int i = 0; i < temp3.size(); i++)
+			{
+				if (temp3[i] == temp)
+				{
+					temp3.erase(temp3.begin() + i);
+				}
+			}
+		}
 	}
 	// stop timer
+	stop = clock();
+	elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
+	cout << "Elapsed time for unordered map in ms: " << elapsed << endl;
 
 	// close the opened file
 	fI.close();
 
+	displayMap(m1);
+	displayUnorderedMap(m2);
 	return 0;
 }
